05d0ba2bfaa956f467937afe1240a09752d30a41
Jonne <jonnesaleva@brandeis.edu>
2021-01-20 13:07:09 -0500

    remove ensure_ascii

--- wikidata_helpers.py
+++ wikidata_helpers.py
@@ -96,7 +96,7 @@ class WikidataRecord:
             return self.record
 
     def to_json(self, simple=True) -> str:
-        return orjson.dumps(self.to_dict(simple), ensure_ascii=False)
+        return orjson.dumps(self.to_dict(simple))
 
     def __str__(self) -> str:
         return f'WikidataRecord(name="{self.name}", id="{self.id}, mongo_id={self.mongo_id} instance_of={self.instance_ofs})"'
