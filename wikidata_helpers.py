ca1a64f5455b5873f46880bba5c1f82a51d7869e
Jonne <jonnesaleva@brandeis.edu>
2021-01-24 18:07:05 -0500

    add WikidataMongoDB

--- wikidata_helpers.py
+++ wikidata_helpers.py
@@ -3,6 +3,7 @@ import orjson
 import os
 
 from typing import Generator, Set, List
+from pymongo import MongoClient
 
 
 class WikidataDump:
@@ -13,6 +14,7 @@ class WikidataDump:
         n_decode_errors = 0
         with bz2.open(self.dumpfile, mode="rt") as f:
             f.read(2)  # skip first two bytes: "{\n"
+
             for line in f:
                 try:
                     yield orjson.loads(line.rstrip(",\n"))
@@ -21,6 +23,21 @@ class WikidataDump:
                     continue
         print(f"Decode errors: {n_decode_errors}")
             
+
+
+class WikidataMongoDB:
+    """Class for interfacing with Wikidata dump
+    ingested into a MongoDB instance."""
+
+    def __init__(
+        self,
+        database_name: str = "wikidata_db",
+        collection_name: str = "wikidata",
+    ) -> None:
+        self.database_name = database_name
+        self.collection_name = collection_name
+        self.client = MongoClient()
+        self.conn = self.client[self.database_name][self.collection_name]
 
 
 class WikidataRecord:
@@ -38,25 +55,30 @@ class WikidataRecord:
         try:
             self.instance_ofs = set(
                 iof["mainsnak"]["datavalue"]["value"]["id"]
+
                 for iof in self.record["claims"]["P31"]
             )
         except KeyError:
             self.instance_ofs = set()
 
     def parse_aliases(self) -> None:
-        self.aliases = {lang: d["value"] for lang, d in self.record["labels"].items()}
+        self.aliases = {
+            lang: d["value"] for lang, d in self.record["labels"].items()
+        }
 
     @property
     def name(self) -> str:
         try:
             if not hasattr(self, "_name"):
                 self._name = self.aliases[self.default_lang]
+
             return self._name
         except KeyError:
             return self.id
 
     def instance_of(self, classes: Set[str]) -> bool:
         """Checks whether the record is an instance of a set of classes"""
+
         return len(self.instance_ofs.intersection(classes)) > 0
 
     def to_dict(self, simple=True) -> dict:
