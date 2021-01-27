027cba14e8a34c4b99849c26bbb401e7445d7369
Jonne <jonne@jonnesaleva.com>
2021-01-27 11:03:30 -0500

    add JS MongoDB query to create instance-of field

---
+++ create_instance_of.js
@@ -0,0 +1,33 @@
+var instanceOfPipeline = [
+    {$unwind: "$claims.P31"}, 
+    {
+        $addFields: {
+            instance_of: "$claims.P31.mainsnak.datavalue.value.id"
+        }
+    }, 
+    {
+        $group: {
+            _id: "$_id", 
+            "instance_of": {
+                $push: "$instance_of"
+            },
+            "doc": { $first: '$$ROOT' }   
+        }
+    },
+    {
+        $replaceRoot: {
+            "newRoot": { 
+                "$mergeObjects": [
+                    "$doc", 
+                    {instance_of: "$instance_of"}
+                ]}
+            }
+    },
+    {
+        $out: {
+            db: "wikidata_db", "coll": "test_instance_of"
+        }
+    }
+];
+
+db.wikidata.aggregate(instanceOfPipeline, {allowDiskUse: true})
