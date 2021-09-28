edc6c6bb9032a1a1fbfbf83b65dba6e30b00bcb6
Jonne Saleva <jonnesaleva@brandeis.edu>
2021-09-28 11:55:20 -0400

    Remove unnecessary scripts

--- recipes/bulk_insert_example.sh
+++
@@ -1,5 +0,0 @@
-python paranames/io/wikidata_bulk_insert.py \
-    -d /mnt/storage/data/wikidata/latest-all.json \
-    --database-name wikidata_db \
-    --collection-name wikidata_simple2 \
-    -w 16 -c 5000 --simple-records --debug
