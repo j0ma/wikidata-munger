cbcd27baa67513ddf91bf9d4f468e52622e27e57
Jonne Saleva <jonnesaleva@brandeis.edu>
2022-02-28 09:20:12 -0500

    bunch of things

--- paranames/analysis/script_entropy_with_cache.sh
+++ paranames/analysis/script_entropy_with_cache.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-HUMAN_READABLE_LANGS_PATH="$HOME/paranames/data/human_readable_lang_names.json"
+HUMAN_READABLE_LANGS_PATH="$HOME/paranames/data/human_readable_lang_names_from_sparql.json"
 
 usage () {
     echo "Usage: bash ./script_analysis.sh TSV_PATH OUTPUT_FILE ALIAS_COL_IX LANG_COL_IX"
@@ -11,11 +11,16 @@ usage () {
 [ $# -lt 2 ] && usage && exit 1
 
 analyze_most_common () {
+
+    ## get number of lines
+    nlines=$(wc -l $DUMP | cut -f1 -d' ')
+
     ## get all unique aliases
-    tail +2 $DUMP | cut -f $ALIAS_COL_IX | sort | uniq > $ALIASES
+    tail +2 $DUMP | cut -f $ALIAS_COL_IX | tqdm --total=$nlines | sort | uniq > $ALIASES
+    n_aliases=$(wc -l $ALIASES | cut -f1 -d' ')
 
     ## infer all paranames
-    ./paranames/analysis/infer_script_most_common < $ALIASES > $SCRIPTS
+    cat $ALIASES | tqdm --total $n_aliases | ./paranames/analysis/infer_script_most_common > $SCRIPTS
 
     ## combine into big flie
     paste $ALIASES $SCRIPTS > $TSV
@@ -44,6 +49,6 @@ TSV=$TMPDIR/all_aliases_with_script.tsv
 
 
 ## get unique languages
-LANGUAGES=$(tail +2 $DUMP | cut -f $LANG_COL_IX | sort | uniq | tr '\n' ' ')
+#LANGUAGES=$(tail +2 $DUMP | cut -f $LANG_COL_IX | sort | uniq | tr '\n' ' ')
 
 analyze_most_common
