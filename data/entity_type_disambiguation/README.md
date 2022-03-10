30ab7ce50c82512fb7e72a55022f8305f2ed4ba0
Jonne Saleva <jonnesaleva@brandeis.edu>
2022-03-10 16:23:02 -0500

    large data folder sync

--- data/entity_type_disambiguation/README.md
+++
@@ -1,5 +0,0 @@
-Taken from 
-
-```
-/home/jonne/datasets/wikidata/per-loc-org-everything/combined/combined.tsv
-```
--- data/name_reversal/cross_alignments/analyze_results.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-folder=$1
-
-csvjoin -t -c 'language_long' $folder/*.tsv 
